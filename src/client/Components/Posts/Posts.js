9b8d69dd1ba87fb0529b92c859a3835be197b9c1
Yodel-oh-ee-dee <42037023+Cheg-ya@users.noreply.github.com>
2019-03-07 16:52:26 +0900

    build posts page UI and post UI

--- src/client/Components/Posts/Posts.js
+++ src/client/Components/Posts/Posts.js
@@ -4,30 +4,100 @@ import { connect } from 'react-redux';
 import './Posts.css';
 import axios from 'axios';
 import Header from '../Header/Header';
+import postsData from './posts.json';
 
 class Posts extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      posts: [],
       displayModal: false
     };
+
+    this.displayPosts = this.displayPosts.bind(this);
   }
 
   componentDidMount() {
     const { posts, onPostsComponentMount } = this.props;
 
-    if (posts.length) {
-      onPostsComponentMount();
+    if (!posts.length) {
+      // onPostsComponentMount();
+      this.setState(() => {
+        return {
+          posts: postsData
+        };
+      });
     }
   }
 
+  displayPosts() {
+    const { posts } = this.state;
+
+    return posts.map((post, i) => { //close filtering
+      const { by, created_at, title, description, reviewers, stack, profile_image } = post;
+      const convertDateType = new Date(created_at).toString().split(' ');
+      const date = `${convertDateType[2]} ${convertDateType[1]} ${convertDateType[3]}`;
+
+      let reviewerState = '';
+
+      if (reviewers.length > 2) {
+        reviewerState = `${reviewers[0]} & ${reviewers.length - 1} others reviewing`;
+      } else if(reviewers.length === 2) {
+        reviewerState = `${reviewers[0]} & ${reviewers[1]} others reviewing`;
+      } else {
+        reviewerState = `${reviewers[0]} others reviewing`;
+      }
+
+      return (
+        <div className="publicPostContainer" key={i}>
+          <div className="publicPostCover">
+            <div className="postBy">
+              <div className="authorProfileCover">
+                <img className="authorProfileImage" src="./public/main.jpg" alt="" />
+              </div>
+              <div className="postDateInfo">
+                <div className="author">{by}</div>
+                <div className="created_at">{date}</div>
+              </div>
+            </div>
+            <div className="postTitle">{title}</div>
+            <p className="postDescription">{description}</p>
+            <div className="reviewerState">{reviewerState}</div>
+          </div>
+          <div className="postReviewInfo">
+            <div className="stackTags">{stack.join(' ')}</div>
+          </div>
+          <div className="reviewBtnCover">
+            <button className="reviewStartBtn">Review Start</button>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
+    const { posts } = this.state;
+
     return (
       <Fragment>
         <Header history={this.props.history} />
-        <div className="postsContainer">
-          <div className="postsCover">
-          123
+        <div className="dump"></div>
+        <div className="wrapper">
+          <div className="newPostContainer">
+            <div className="header">
+              <span>Create Review Post</span>
+            </div>
+            <div className="postInfoContainer">
+              <div className="profileCover">
+                <img className="profileImage" src="./public/main.jpg" alt="" />
+              </div>
+              <div className="message">
+                <span>What is the matter with your code?</span>
+              </div>
+            </div>
+          </div>
+          <div className="postsWrapper">
+            {posts.length > 0 && this.displayPosts()}
           </div>
         </div>
       </Fragment>
